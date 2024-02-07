dd29b18f8d3f8be538ed99229523efacbe8490b6
mayfloq <159261244+mayfloq@users.noreply.github.com>
2024-02-07 07:27:25 -0500

    Add files via upload

---
+++ Dr4E8A1Application.java
@@ -0,0 +1,13 @@
+package br.edu.infnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr4E8A1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr4E8A1Application.class, args);
+	}
+
+}
