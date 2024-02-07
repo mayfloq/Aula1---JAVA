4ae70bfd87bdaaff16c0d1746d307cd28bf0ef38
mayfloq <159261244+mayfloq@users.noreply.github.com>
2024-02-07 07:27:00 -0500

    Add files via upload

---
+++ Dr4E7A2CandidatoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dr4E7A2CandidatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
